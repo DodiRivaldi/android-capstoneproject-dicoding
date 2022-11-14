b3c081dfd705e2d5effe6e59b612bc2f0b4f12d8
Dodi <dodirivaldi64@gmail.com>
2022-11-14 11:33:26 +0700

    Modify func DetailViewModel()

--- app/src/main/java/com/dodi/androidbaseproject/features/detail/DetailViewModel.kt
+++ app/src/main/java/com/dodi/androidbaseproject/features/detail/DetailViewModel.kt
@@ -1,8 +1,23 @@
 package com.dodi.androidbaseproject.features.detail
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.dodi.core.data.domain.team.TeamUseCase
+import com.dodi.core.data.model.TeamModel
 import javax.inject.Inject
 
 class DetailViewModel @Inject constructor(private val useCase: TeamUseCase?): ViewModel() {
+    private val _item = MutableLiveData<TeamModel>()
+    val item: LiveData<TeamModel> = _item
+    val isFavorite: LiveData<Boolean> = Transformations.switchMap(_item){
+        useCase?.isFavorite(it)?.asLiveData()
+    }
+    fun setSelected(teamModel: TeamModel){
+        _item.postValue(teamModel)
+    }
+    fun insertFavorite(state : Boolean){
+        _item.value?.let {
+            useCase?.insertFavorite(it,state)
+            _item.postValue(it)
+        }
+    }
 }
